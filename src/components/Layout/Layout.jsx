6e7e6d19824eb14067241123b381712994b49330
Aliaa <aliaamohamaddahi@gmail.com>
2026-09-10 01:28:57 +0300

    make register design

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -6,7 +6,9 @@ const Layout = () => {
   return (
     <div>
       <Navbar />
-      <Outlet />
+      <div className="pt-20">
+        <Outlet />
+      </div>
       <Footer />
     </div>
   );
--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
 
 const Signup = () => {
   return (
-    <div>
-      
-    </div>
+   
+
+<form className="w-8/12 mx-auto">
+  <div className="mb-5">
+    <label htmlFor="name" className="block mb-2.5 text-sm font-medium text-heading">Your name</label>
+    <input type="text" id="name" name="name" className=" bg-neutral-secondary-medium border border-default-medium text-sm rounded-base focus:outline-none focus:ring-2 focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" />
+  </div>
+
+   <div className="mb-5">
+    <label htmlFor="email" className="block mb-2.5 text-sm font-medium text-heading">Your email</label>
+    <input type="email" id="email" name="email" className=" bg-neutral-secondary-medium border border-default-medium text-sm rounded-base focus:outline-none focus:ring-2 focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" />
+  </div>
+
+  <div className="mb-5">
+    <label htmlFor="password" className="block mb-2.5 text-sm font-medium text-heading">Your name</label>
+    <input type="password" id="password" name="password" className=" bg-neutral-secondary-medium border border-default-medium text-sm rounded-base focus:outline-none focus:ring-2 focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" />
+  </div>
+
+<div className="mb-5">
+    <label htmlFor="rePassword" className="block mb-2.5 text-sm font-medium text-heading">Your name</label>
+    <input type="password" id="rePassword" name="rePassword" className=" bg-neutral-secondary-medium border border-default-medium text-sm rounded-base focus:outline-none focus:ring-2 focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" />
+  </div>
+
+<div className="mb-5">
+    <label htmlFor="phone" className="block mb-2.5 text-sm font-medium text-heading">Your name</label>
+    <input type="tel" id="phone" name="phone" className=" bg-neutral-secondary-medium border border-default-medium text-sm rounded-base focus:outline-none focus:ring-2 focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" />
+  </div>
+
+  <button type="submit" className="text-white bg-brand box-border border border-transparent hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">Submit</button>
+</form>
+
+
+
   )
 }
 
