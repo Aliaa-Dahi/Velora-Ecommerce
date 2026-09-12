a6eaac9ee0226713e8918e18be365c479aa523ed
Aliaa <aliaamohamaddahi@gmail.com>
2026-09-12 23:18:52 +0300

    add a loader and handle it's styling

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,8 +7,10 @@ const Home = () => {
   let [productList, setProductList] = useState([]);
   let [currentPage, setCurrentPage] = useState(1);
   let [numberOfPages, setNumberOfPages] = useState(0);
+  let [isLoading, setIsLoading] = useState(true);
 
   function getAllProducts(page = 1) {
+    setIsLoading(true);
     axios
       .get(
         `https://ecommerce.routemisr.com/api/v1/products?page=${page}&limit=12`
@@ -18,6 +20,7 @@ const Home = () => {
         setNumberOfPages(res.data.metadata.numberOfPages);
         setCurrentPage(page);
         setProductList(res.data.data);
+        setIsLoading(false);
       })
       .catch((err) => {
         console.log(err);
@@ -27,73 +30,88 @@ const Home = () => {
     getAllProducts();
   }, []);
   return (
-    <div className="w-11/12 mx-auto">
-      <div className="flex flex-wrap -mx-2">
-        {productList?.map((product) => {
-          return (
-            <div key={product.id} className="w-2/12 px-2 mb-4">
-              <div className="item border border-brand p-4 group overflow-hidden">
-                <div className="relative overflow-hidden mb-1">
-                  <img
-                    className="w-full block"
-                    src={product.imageCover}
-                    alt={product.title}
-                  />
-                  <button className="btn absolute bottom-0 left-0 w-full translate-y-full group-hover:translate-y-0 duration-200">
-                    Add To Cart
-                  </button>
-                </div>
-                <h5 className="text-brand text-sm">{product.category.name}</h5>
-                <h2 className="text-lg">
-                  {product.title.split(" ").slice(0, 2).join(" ")}
-                </h2>
-                <div className="flex justify-between">
-                  <span>{product.price}</span>
-                  <span>
-                    {product.ratingsAverage}
-                    <FontAwesomeIcon
-                      icon={faStar}
-                      className="text-yellow-500"
-                    />
-                  </span>
-                </div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
+    <>
+      {isLoading ? (
+        <div className="w-full flex justify-center translate-y-50">
+          <span className="loader mx-auto"></span>
+        </div>
 
-      <nav aria-label="Page navigation example">
-        <ul className="flex -space-x-px text-sm justify-center">
-          <li>
-            <a
-              onClick={() => currentPage > 1 && getAllProducts(currentPage - 1)}
-              className="flex items-center justify-center text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading font-medium rounded-s-base text-sm px-3 h-9 focus:outline-none cursor-pointer"
-            >
-              Previous
-            </a>
-          </li>
-          {Array(numberOfPages)
-            .fill(null)
-            .map((_, index) => {
+      ) : (
+        <div className="w-11/12 mx-auto">
+          <div className="flex flex-wrap -mx-2">
+            {productList?.map((product) => {
               return (
-                <li key={index} onClick={() => getAllProducts(index + 1)}>
-                  <a className="flex items-center justify-center text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading font-medium text-sm px-3 h-9 focus:outline-none cursor-pointer">
-                    {index + 1}
-                  </a>
-                </li>
+                <div key={product.id} className="w-2/12 px-2 mb-4">
+                  <div className="item border border-brand p-4 group overflow-hidden">
+                    <div className="relative overflow-hidden mb-1">
+                      <img
+                        className="w-full block"
+                        src={product.imageCover}
+                        alt={product.title}
+                      />
+                      <button className="btn absolute bottom-0 left-0 w-full translate-y-full group-hover:translate-y-0 duration-200">
+                        Add To Cart
+                      </button>
+                    </div>
+                    <h5 className="text-brand text-sm">
+                      {product.category.name}
+                    </h5>
+                    <h2 className="text-lg">
+                      {product.title.split(" ").slice(0, 2).join(" ")}
+                    </h2>
+                    <div className="flex justify-between">
+                      <span>{product.price}</span>
+                      <span>
+                        {product.ratingsAverage}
+                        <FontAwesomeIcon
+                          icon={faStar}
+                          className="text-yellow-500"
+                        />
+                      </span>
+                    </div>
+                  </div>
+                </div>
               );
             })}
-          <li
-            onClick={() => currentPage < numberOfPages && getAllProducts(currentPage + 1)}
-          >
-            <a className="flex items-center justify-center text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading font-medium rounded-e-base text-sm px-3 h-9 focus:outline-none cursor-pointer">
-              Next
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </div>
+          </div>
+
+          <nav aria-label="Page navigation example">
+            <ul className="flex -space-x-px text-sm justify-center">
+              <li>
+                <a
+                  onClick={() =>
+                    currentPage > 1 && getAllProducts(currentPage - 1)
+                  }
+                  className="flex items-center justify-center text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading font-medium rounded-s-base text-sm px-3 h-9 focus:outline-none cursor-pointer"
+                >
+                  Previous
+                </a>
+              </li>
+              {Array(numberOfPages)
+                .fill(null)
+                .map((_, index) => {
+                  return (
+                    <li key={index} onClick={() => getAllProducts(index + 1)}>
+                      <a className="flex items-center justify-center text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading font-medium text-sm px-3 h-9 focus:outline-none cursor-pointer">
+                        {index + 1}
+                      </a>
+                    </li>
+                  );
+                })}
+              <li
+                onClick={() =>
+                  currentPage < numberOfPages && getAllProducts(currentPage + 1)
+                }
+              >
+                <a className="flex items-center justify-center text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading font-medium rounded-e-base text-sm px-3 h-9 focus:outline-none cursor-pointer">
+                  Next
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      )}
+    </>
   );
 };
 
